6499e0ff5b5c999e91171883ea814d9719854eaf
Omlokhande1234 <olokhande56@gmail.com>
2024-06-14 16:39:59 +0530

    "Updated paymentController.js to add verification of Razorpay payment, cancel subscription functionality, and refund logic"

--- Server/Controllers/paymentController.js
+++ Server/Controllers/paymentController.js
@@ -3,6 +3,8 @@ import User from "../Models/userModel.js";
 import { razorPay } from "../index.js";
 import { errorhandler } from "../utils/errorHandler.js";
 import dotenv from 'dotenv'
+import crypto from 'crypto'
+import { createDeflate } from "zlib";
 dotenv.config()
 export const getRazorpayKey=async(req,res,next)=>{
     try{
@@ -57,7 +59,42 @@ export const BuySubscription=async(req,res,next)=>{
 }
 export const verifySubscription=async(req,res,next)=>{
     try{
-        
+        //Get the Id of the user
+        const{id}=req.user
+        //Get payment details of razorpay such as id, subscription_id and signature from the body
+        //This stuff we will get when the user hit on to the payment
+        const{razorpay_payment_id,razorpay_subscription_id,razorpay_signature}=req.body
+        //Find the user 
+        const user=await User.findById(id)
+        //Get subscription id of the user from db
+        const Subscription_Id=user.subscription.id
+        //Create the new signature using the subscription id of the user and payment id we took from
+        //the body 
+        const generatedSignature=await crypto
+              .createHmac('sha256',process.env.RAZORPAY_SECRET)
+              .update(`${razorpay_payment_id}|${Subscription_Id}`)
+              .digest('hex')
+        //Check whether the generatedSignature is same as to that of the payment signature
+        //If its not same then return Payment not verified
+        if(!razorpay_signature==generatedSignature){
+            return next(errorhandler(400,"Payment not verified,please try again"))
+        }
+        //Here if the payMent is verified i.e our generatedSignature==razorpay_signature then
+        //just create the payment in the database as shown below
+        await Payment.create({
+            razorpay_payment_id,
+            razorpay_subscription_id,
+            razorpay_signature
+        })
+        //change the subscription status of the user as active
+        user.subscription.status='active'
+        //at the send save the user in the database 
+        await user.save()
+
+        return res.status(200).json({
+            success:true,
+            message:"Payment is verified"
+        })
 
     }
     catch(error){
@@ -65,11 +102,53 @@ export const verifySubscription=async(req,res,next)=>{
     }
 }
 export const cancelSubscription=async(req,res,next)=>{
+    //Take the id of the user
+    const{id}=req.user
     try{
+        //Find the user with the given id
+       const user=await User.findById(id)
+       //Check who is trying to unsubscribe the subscription
+       if(user.role=='ADMIN'){
+        return next(errorhandler(400,"Admin cant unsubscribe"))
+       }
+       //Find out the subscription id from the user
+       const SubscriptionId=user.subscription.id
+       //Delete the subscription of the user with given subscription_id
+       const Subscription=await razorPay.subscriptions.cancel(
+        SubscriptionId
+       )
+       //Update the subscription status
+       user.subscription.status=Subscription.status
+       //save the user with updated status
+       await user.save()
+       //Now find the payment corresponding to given subscriptionId
+       const payment=await Payment.findOne({
+        razorpay_subscription_id:SubcriptionId
+       })
+       //Find the time subscribed 
+       const timeSubscribed=Date.now()-createdAt
+       const refundPeriod=14*24*60*60*1000
+       //Check whether the refund period is less or greater than timeSubscribed 
+       if(refundPeriod<=timeSubscribed){
+        return next(errorhandler(400,"Refund not available as the refund period is over"))
+       }
+       //if the timeSubscribed is in the refundPeriod then just initiate the refund
+       await razorPay.payments.refund(payment.razorpay_payment_id)({
+        speed:'optimum'
+       })
+       user.subscription.id=undefined
+       user.subscription.status=undefined
 
+       await user.save()
+       await payment.remove()
+       return res.status(200).json({
+            success:true,
+            message:"Unsubscribe successfully"
+        })
     }
     catch(error){
-        return next(errorhandler(400,error.message))
+        console.log(error)
+        return next(errorhandler(error.statusCode,error.error.description))
     }
 }
 export const allPayments=async(req,res,next)=>{
